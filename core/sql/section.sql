9a49c3a38bc6c48161a1d0a4a167a1d4f5219ba3
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2008-08-02 19:34:12 +0000

    deleting files

--- core/sql/section.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO core_section (name) VALUES ('News');
-INSERT INTO core_section (name) VALUES ('Sports');
-INSERT INTO core_section (name) VALUES ('Ed');
-INSERT INTO core_section (name) VALUES ('Arts');
-INSERT INTO core_section (name) VALUES ('FM');
--- core/sql/tag.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO core_tag (text, is_nav) VALUES ('Science', 'false');
-INSERT INTO core_tag (text, is_nav) VALUES ('Harvard', 'false');
-INSERT INTO core_tag (text, is_nav) VALUES ('Faust', 'false');
-INSERT INTO core_tag (text, is_nav) VALUES ('IT', 'false');
-INSERT INTO core_tag (text, is_nav) VALUES ('Grapes', 'false');
-INSERT INTO core_tag (text, is_nav) VALUES ('Graduation', 'false');
