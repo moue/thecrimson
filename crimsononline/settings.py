bbfed131c526bc41d6a88fcbe53d32065ea3ca17
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2008-08-03 00:33:27 +0000

    moved admin media prefix to local settings

--- crimsononline/settings.py
+++ crimsononline/settings.py
@@ -32,11 +32,6 @@ MEDIA_ROOT = ''
 # Example: "http://media.lawrence.com"
 MEDIA_URL = ''
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
 #    'django.template.loaders.filesystem.load_template_source',
