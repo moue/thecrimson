030df4a840206f668f2015588c4a0f773f371c49
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2008-08-19 02:44:25 +0000

    image gallery - image selection works

--- crimsononline/core/admin.py
+++ crimsononline/core/admin.py
@@ -80,10 +80,12 @@ admin.site.register(Image, ImageAdmin)
 
 class ImageSelectMultipleWidget(forms.widgets.SelectMultiple):
     def render(self, name, value, attrs=None, choices=()):
-        output = '<div id="images-bank"><ul id="image-list"></ul></div>\n'
+        output = '<div id="images-bank"><ul class="image-list"></ul>' \
+                    '<div class="links"></div></div>\n'
         output += super(ImageSelectMultipleWidget, self). \
             render(name, value, attrs, choices)
-        output += '<div id="image-current"><ul id="current-image-list"></ul></div>'
+        output += '<div id="images-current"><h3>Images in this ' \
+                    'Image Gallery</h3><ul class="image-list"></ul></div>'
         return mark_safe(output)
 
 
@@ -95,8 +97,9 @@ class ImageSelectModelMultipleChoiceField(forms.ModelMultipleChoiceField):
     def clean(self, value):
         qs = self.queryset
         self.queryset = Image.objects
-        return super(ImageSelectModelMultipleChoiceField, self).clean(value)
+        c = super(ImageSelectModelMultipleChoiceField, self).clean(value)
         self.queryset = qs
+        return c
 
 
 class ImageGalleryForm(ModelForm):
--- crimsononline/static/scripts/admin/ImageGallery.js
+++ crimsononline/static/scripts/admin/ImageGallery.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    //TODO: hide the SelectMultiple field
+    //hide the SelectMultiple field
+    $("#id_images").hide();
     
     // prefix of the url from which to grab images
-    var image_req_url = "/admin/core/imagegallery/get_images/page/";
+    var image_req_url = "/admin/core/imagegallery/get_images/";
     // which pks are already in the SelectMultiple field
     var choices = function(){
-       var selects = $(id_images).children();
+       var selects = $("#id_images").children();
        var pks = [];
        if(selects != null){
-           var pks = [];
            $.each(selects, function(i){
                pks.push($(this).val());
            });
@@ -19,7 +19,7 @@ $(document).ready(function(){
     // images cache
     var img_cache = {by_pk: {}};
     var addToCache = function(page_num, page){
-        images = {}
+        var images = {}
         $.each(page.images, function(pk, img){
             // convert each html fragment into an element
             images[pk] = $(img);
@@ -27,80 +27,139 @@ $(document).ready(function(){
         });
         page.images = images;
         img_cache[page_num] = page;
+        //testing
+        $.each(img_cache.by_pk, function(i, item){
+            console.log(i + "\n" + item.html());
+        })
     }
     
-    //adds image with in element ele to the current list of images
-    //   and removes it from the bank of images
-    var addImgToList = function(ele){
-        //adds image to the select field (if it isn't in there already)
-        var pk = $(ele).attr("img_pk");
+    //adds image with in element ele to images-current
+    //   and removes it from the images-bank
+    var addImgToCurrent = function(ele){
+        ele = $(ele);
+        console.log(ele.html());
+        //if the image isn't in the select field, add it
+        var pk = ele.attr("img_pk");
         if(choices.indexOf(pk) == -1){
-            var capt = $(ele).children(".caption").html();
+            var capt = ele.children(".caption").html();
             $("<option value=\"" + pk + "\" selected=\"selected\">" 
                 + capt + "</option>")
                 .appendTo("#id_images");
+            //add the pk to choices
+            choices.push(pk);
+        // if its already there, select it
+        } else {
+            $("#id_images").children("[value='" + pk + "']")
+                .attr("selected", "selected");
         }
-        //fade ele and unbind its event
-        $(ele).unbind().fadeTo("slow", 0.3);
+        //fade ele and unbind its event; mark it as selected
+        ele.unbind().fadeTo("slow", 0.3);
         img_cache.by_pk[pk].addClass("selected");
+        //clone ele and add it to images-current
+        ele.clone()
+            .appendTo("#images-current .image-list")
+            .hide()
+            .click(function(){
+                removeImgFromCurrent(this);
+            })
+            .fadeIn("slow");
     }
     
-    //update bank of images that are current selected
-    var updateCurImages = function(){
-        $.each($("#id_images").val(), function(i){
-            
+    //removes image ele from images-current and adds it back to images-bank
+    var removeImgFromCurrent = function(ele){
+        ele = $(ele);
+        var pk = ele.attr("img_pk");
+        // deselect in the select field
+        $("#id_images").children("[value='" + pk + "']")
+            .removeAttr("selected");
+        //remove the image
+        ele.fadeOut("slow", function(){
+            $(this).remove();
         });
+        // if its in the cache, unfade it and rebind its event handler
+        img = img_cache.by_pk[pk];
+        if(img != null){
+            img
+                .fadeTo("slow", 1.0, function(){
+                    $(this).click(function(){
+                        addImgToCurrent(this);
+                    });
+                })
+                .removeClass("selected");
+        }
     }
     
-    // take remove old images from selection list, add the ones from cache
-    //    to the selection list
-    var showImages = function(page_num){
+    // remove old images from images-bank, add the ones from cache
+    var updateImagesBank = function(page_num){
         cur_page = img_cache[page_num]
         if(cur_page != null){
-            //clear out the old image list
-            $("ul#image-list").empty();
+            //clear out the images from the images-bank
+            var imglist = $("#images-bank ul.image-list");
+            imglist.empty();
             //add new images and wire them to their click handlers
             $.each(cur_page["images"], function(pk, img){
-                img.appendTo("ul#image-list")
+                img.appendTo(imglist)
                 // don't wire images that are already selected
                 if(!img.hasClass("selected")){
                     img.click(function(){
-                        addImgToList(this);
+                        addImgToCurrent(this);
                     });
                 }
             })
-            //remove old links and add new ones (and wire their ev handlers)
-            $("a.images-bank").remove();
+            //remove old links, add new ones, and wire their ev handlers
+            $("#images-bank .links").empty();
             if(cur_page.next_page != 0){
-                $("<a href=\"#\" class=\"images-bank\">Older</a>")
-                    .appendTo("ul#image-list")
-                    .click(function(){getImages(cur_page.next_page)})
+                $("<a href=\"#\">Older</a>")
+                    .appendTo("#images-bank .links")
+                    .click(function(){getImagesBank(cur_page.next_page)})
                     .append($("<span> | </span>"));
             }
             if(cur_page.prev_page != 0){
-                $("<a href=\"#\" class=\"images-bank\">Newer</a>")
-                    .appendTo("ul#image-list")
-                    .click(function(){getImages(cur_page.prev_page)});            
+                $("<a href=\"#\">Newer</a>")
+                    .appendTo("#images-bank .links")
+                    .click(function(){getImagesBank(cur_page.prev_page)});            
             }
         }
     }
     
-    // makes ajax request for images
-    var getImages = function(page_num){
+    // makes ajax request for images for the images-bank
+    var getImagesBank = function(page_num){
         // if its not in the cache, make the request and load it into the cache
         if(img_cache[page_num] == null){
-            $.getJSON(image_req_url + page_num, function(json){
+            $.getJSON(image_req_url + "page/" + page_num, function(json){
                 //put it in the cache
                 addToCache(page_num, json);
                 //remove old images, add new ones
-                showImages(page_num);
+                updateImagesBank(page_num);
             })
+        // otherwise just trigger the AJAX-received action
         } else {
-            showImages(page_num);
+            updateImagesBank(page_num);
         }
     }
     
-    // make first ajax request
-    getImages(1);
-    updateCurImages();
+    // makes ajax request for images for images-current
+    var getImagesCur = function(){
+        $.each(choices, function(i){
+            // grab the image, wire its click handler
+            $.get(image_req_url + "pk/" + this, function(html){
+                var ele = $(html);
+                var pk = ele.attr("img_pk");
+                ele
+                    .appendTo("#images-current .image-list")
+                    .click(function(){
+                        removeImgFromCurrent(this);
+                    });
+                // if its in the cache, fade it from images-bank
+                ele = img_cache.by_pk[pk];
+                if(ele != null){
+                    $(ele).fadeTo("fast", 0.3).unbind();
+                }
+            });
+        });
+    }
+    
+    // make first ajax requests
+    getImagesBank(1);
+    getImagesCur();
 });
