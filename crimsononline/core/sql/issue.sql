af3c46cff3a5798df561246df5c64d8ed7c31260
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2008-09-21 06:47:26 +0000

    added fixtures, removed sql from core

--- crimsononline/core/sql/issue.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO core_issue
-	(web_publish_date, issue_date)
-VALUES
-	('2008-07-23 00:00:00', '2008-07-23');
-	
-INSERT INTO core_issue
-	(web_publish_date, issue_date)
-VALUES
-	('2008-07-21 00:00:00', '2008-07-21');
--- crimsononline/core/sql/tag.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO core_tag (text) VALUES ('it');
-INSERT INTO core_tag (text) VALUES ('harvard');
-INSERT INTO core_tag (text) VALUES ('the crimson');
-INSERT INTO core_tag (text) VALUES ('college');
-INSERT INTO core_tag (text) VALUES ('iraq');
-INSERT INTO core_tag (text) VALUES ('drinking');
-INSERT INTO core_tag (text) VALUES ('hygiene');
