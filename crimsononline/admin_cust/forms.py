ea1d634e30b1cad7e5d358ebcdfbeffb5b348fba
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2008-10-21 15:09:23 +0000

    some docstrings

--- crimsononline/admin_cust/forms.py
+++ crimsononline/admin_cust/forms.py
@@ -3,6 +3,16 @@ from django.conf import settings
 from django.template.loader import render_to_string
 
 class FbSelectMultipleWidget(forms.widgets.HiddenInput):
+    """
+    A widget that allows for multiple model selection, similar to Facebook's
+    compose message To: field.
+    
+    Takes 3 additional parameters
+    url: 
+    model: 
+    Labeler: 
+    For explanations, see FbModelMultipleChoiceField's docstring
+    """
     class Media:
         js = ("/site_media/scripts/framework/jquery.bgiframe.min.js",
             "/site_media/scripts/framework/jquery.dimensions.js",
@@ -31,6 +41,22 @@ class FbSelectMultipleWidget(forms.widgets.HiddenInput):
         return render_to_string("widgets/fb_select_multiple.html", locals())
         
 class FbModelMultipleChoiceField(forms.CharField):
+    """
+    A model multiple choice field that uses a Facebook-like autocomplete
+    interface / widget.
+    
+    This field is bound to the FbSelectMultiple Widget.
+    
+    Takes 3 additioal arguments:
+    model: the model from which to select multiple
+    url: the widget makes a AJAX requests for autocompletion. this is the
+        url that returns the autcomplete search results. the request is made
+        to the url: url?q=query_string&limit=max_results
+        the results should be returned as text, with one result on each
+        line. each results should look like: primary_key|label
+    labeler: a function that takes a model object and returns a label.
+        this is used for the initial labels.
+    """
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model')
         kwargs['widget'] = FbSelectMultipleWidget(url=kwargs.pop('url'), 
