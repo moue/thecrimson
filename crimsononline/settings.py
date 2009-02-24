340e499e00e81d80de456d44dd223d1fa7896165
krakip <krakip@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-02-24 03:01:26 +0000

    Auth backend added

--- crimsononline/settings.py
+++ crimsononline/settings.py
@@ -30,6 +30,9 @@ TIME_ZONE = 'America/Chicago'
 # *********** THIS IS BROKEN RIGHT NOW *************
 AUTH_PROFILE_MODULE = 'core.UserData'
 
+AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',
+                           'crimsononline.core.admin.HUIDBackend',)
+
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
