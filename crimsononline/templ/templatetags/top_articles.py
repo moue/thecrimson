70a8b5a99b1c525ac491dc3fd823d7c456df1a12
krakip <krakip@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-03-08 19:57:23 +0000

    Some work for most commented articles

--- crimsononline/templ/templatetags/top_articles.py
+++ crimsononline/templ/templatetags/top_articles.py
@@ -2,8 +2,14 @@ from django import template
 from django.db import connection
 from django.template import Context
 from django.template.loader import get_template
+from django.utils import simplejson
 from django.utils.safestring import mark_safe
 from crimsononline.core.models import Image, Article, Content, ContentGeneric
+import urllib
+
+USER_KEY="vabqI2su93P1wVF3Ls9kXhXhRggV7y2ylokjq137yPAz47cY5dDMHgUA2QlZoWNE"
+# The following is true for now
+FORUM_KEY="9XGWB9o6NT1ZNiMtc2vt6ZJIUdp0D6sZDvis4hPfGGpsRaUchuH5c4fbO71GPAOj"
 
 register = template.Library()
 
@@ -13,13 +19,19 @@ def compile_most_read(parser, token):
     
 class TopArticlesNode(template.Node):
     def render(self, context):
+        # Step 1: We want to get the most viewed articles from the database
         cursor = connection.cursor()
         # SO GAY
         cursor.execute("SELECT DISTINCT core_article.id, " \
                        "(core_contentgeneric.hits * (1/(julianday('now') - julianday(core_article.created_on) + 2))) AS hitindex " \
                        "FROM core_article, core_contentgeneric WHERE core_contentgeneric.object_id = core_article.id ORDER BY hitindex DESC LIMIT 5")
-        toparticles = cursor.fetchall()
-        toparticles = map(lambda x: Article.objects.get(pk=x[0]), toparticles)
+        mostreadarticles = cursor.fetchall()
+        mostreadarticles = map(lambda x: Article.objects.get(pk=x[0]), mostreadarticles)
+        # Step 2: Grab the JSON crap from Disqus and build another list of the most commented articles
+        url = "http://disqus.com/api/get_thread_list/?forum_api_key=" + FORUM_KEY
+        thread_list = simplejson.load(urllib.urlopen(url))
+        # access individual things by means of thread_list['message'][0], etc.
+        
         t = get_template('mostreadarticles.html')
-        html = t.render(Context({'articlelist': toparticles}))
+        html = t.render(Context({'mostreadarticleslist': mostreadarticles}))
         return html
