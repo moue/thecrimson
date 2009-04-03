baa43db654edc8eef32e21a7271fd554e53d4457
krakip <krakip@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-04-03 00:58:17 +0000

    Minor progress on finishing the urlresolver thing--still need to iterate over, resolve URL, throw objects into a list

--- crimsononline/content/templatetags/top_articles.py
+++ crimsononline/content/templatetags/top_articles.py
@@ -13,6 +13,14 @@ USER_KEY="vabqI2su93P1wVF3Ls9kXhXhRggV7y2ylokjq137yPAz47cY5dDMHgUA2QlZoWNE"
 # The following is true for now
 FORUM_KEY="9XGWB9o6NT1ZNiMtc2vt6ZJIUdp0D6sZDvis4hPfGGpsRaUchuH5c4fbO71GPAOj"
 
+generic_obj_patterns = patterns('crimsononline.content.views',
+    url('^' + CONTENT_URL_RE, 'get_content_obj', name='content_content'),
+    url('^' + CGROUP_URL_RE + CONTENT_URL_RE + '$', 'get_grouped_content_obj',
+        name='content_grouped_content'),
+    url('^' + CGROUP_URL_RE + '$', 'get_content_group_obj', 
+        name='content_contentgroup'),
+)
+
 register = template.Library()
 
 @register.tag(name="most_read_articles")
@@ -45,12 +53,18 @@ class TopArticlesNode(template.Node):
         thread_list['message'].sort(lambda x, y: cmp(int(x['comment_index'], y['comment_index'])))
         # Finish this later (need to get articles based on their URL; we can probably grab the ID out of the URL string in the dict
         # mostcommentedarticleslist = map(lambda x: Article.objects.get(
+        resolver = RegexURLResolver(r'^/', generic_obj_patterns);
+        del thread_list['message'][5:]
+        # call resolver.resolve on everything in the list
+        map
+        
         THE PLAN HERE:
         Copy and paste the generic urlpatterns from urls.py.  Create an instance of RegexURLResolver and have it resolve the
         URL with that pattern, giving us the view function that will give us the article object or nothing at all.
         Once we have that info, we can call the view on the parameters (it's not a real view since it doesn't return an
         HTTPResponse) and get the object for the article, which can then be put into the mostcommentedarticles list.
         """
+        
         t = get_template('mostreadarticles.html')
         html = t.render(Context({'mostreadarticleslist': mostreadarticles}))
         return html
