ad8f8e665b2e36cc30bebed8c96cdbd428f91ed7
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-04-05 19:22:37 +0000

    fixed Kent's breaking of the website

--- crimsononline/content/templatetags/top_articles.py
+++ crimsononline/content/templatetags/top_articles.py
@@ -2,12 +2,14 @@ from datetime import datetime, timedelta
 from math import log
 import urllib
 from django import template
+from django.conf.urls.defaults import patterns, url
 from django.db import connection
 from django.template import Context
 from django.template.loader import get_template
 from django.utils import simplejson
 from django.utils.safestring import mark_safe
 from crimsononline.content.models import Image, Article, Content, ContentGeneric
+from crimsononline.urls import CONTENT_URL_RE, CGROUP_URL_RE
 
 USER_KEY="vabqI2su93P1wVF3Ls9kXhXhRggV7y2ylokjq137yPAz47cY5dDMHgUA2QlZoWNE"
 # The following is true for now
--- crimsononline/content/views.py
+++ crimsononline/content/views.py
@@ -10,7 +10,9 @@ from crimsononline.content_module.models import ContentModule
 
 
 def get_content(request, ctype, year, month, day, slug, pk, content_group=None):
-    c = get_content_obj(ctype, pk)
+    #c = get_content_obj(ctype, pk)
+    c = ContentGeneric.objects.get(content_type__name=ctype, object_id=int(pk))
+    c = c.content_object
     return HttpResponse(c._render('page'))
 
 def get_content_obj(request, ctype, year, month, day, slug, pk, content_group=None):
