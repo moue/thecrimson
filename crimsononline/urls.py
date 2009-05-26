ac14f6ab1a6eb571e7af4ab9b687e79e266e1df7
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-05-26 04:00:51 +0000

    fixed smal bug in writer url

--- crimsononline/urls.py
+++ crimsononline/urls.py
@@ -11,7 +11,7 @@ FILTER_URL_RE = r'(?:sections/(?P<section_str>[A-Za-z,]+)/)?' \
     r'(?:types/(?P<type_str>[A-Za-z,\s]+)/)?' \
     r'(?:page/(?P<page>\d+)/)?'
 urlpatterns = patterns('crimsononline.content.views',
-    url(r'writer/(?P<pk>\d+)(?P<f_name>[A-Za-z\s]+)_/' \
+    url(r'writer/(?P<pk>\d+)/(?P<f_name>[A-Za-z\s]+)_' \
         r'(?P<m_name>[A-Za-z]?)_(?P<l_name>[A-Za-z]+)/%s$' % FILTER_URL_RE,
         'writer', name='content_writer_profile'),
     url(r'^section/(?P<section>[A-Za-z]+)/$', 'section', name='content_section'),
