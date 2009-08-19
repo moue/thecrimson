fa1f47fd849d14056a987d2fb7622ac2140fde92
sstelmach <sstelmach@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-08-19 16:28:34 +0000

    Disable searsearch by default

--- crimsononline/settings.py
+++ crimsononline/settings.py
@@ -109,8 +109,7 @@ INSTALLED_APPS = (
     'crimsononline.search',
     'crimsononline.common',
     'crimsononline.subscriptions',
-    'solango',
-    'haystack',
+    #'haystack',
 )
 
 MEDIA_LOC = MEDIA_ROOT
