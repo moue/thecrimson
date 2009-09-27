a38ca56a56b5baa5c887addaa71d0a332cfc9950
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-09-27 17:30:08 +0000

    urls

--- crimsononline/urls.py
+++ crimsononline/urls.py
@@ -7,7 +7,7 @@ from crimsononline.content.views import *
 from crimsononline.admin_cust.views import login_user
 from django.contrib.auth.views import login, logout
 from django.contrib.sitemaps import FlatPageSitemap
-#from crimsononline.content.sitemaps import ArticleSitemap
+from crimsononline.content.sitemaps import ArticleSitemap
 
 admin.autodiscover()
 
