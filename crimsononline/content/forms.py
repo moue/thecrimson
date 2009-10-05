f66d6649d773a0486cf016c0e90d359ec5216871
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-10-05 06:07:45 +0000

    related content ordering

--- crimsononline/content/forms.py
+++ crimsononline/content/forms.py
@@ -228,8 +228,8 @@ class RelatedContentField(forms.CharField):
         """
         if not value:
             return []
-        ids = value.split(';')
-        q = reduce(lambda x, y: x | y, [Q(pk=p) for p in ids])
+        ids = [int(id) for id in value.split(';')]
+        q = reduce(lambda x, y: x | y, [Q(pk=intp) for p in ids])
         objs = list(Content.objects.filter(q))
         # retrieving Content objs MUST preserve their order!!!
         objs = dict([(obj.pk, obj) for obj in objs])
