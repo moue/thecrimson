7d999151562f065c6797f0882600df5f109c57d5
sstelmach <sstelmach@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-11-04 03:06:58 +0000

    Search stuff: indexes, solr config

--- crimsononline/content/search_indexes.py
+++ crimsononline/content/search_indexes.py
@@ -1,4 +1,10 @@
 from haystack import site
+from haystack import indexes
 from crimsononline.content.models import Article
 
-site.register(Article)
+class ArticleIndex(indexes.SearchIndex):
+	text = indexes.CharField(document=True, model_attr='teaser', use_template=True)
+	pub_date = indexes.DateTimeField(model_attr='issue__issue_date')
+	
+
+site.register(Article, ArticleIndex)
