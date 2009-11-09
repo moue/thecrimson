fdd5f0ffea04d326acdcb95feb274306070465c4
andylei <andylei@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-11-09 06:35:13 +0000

    fuck you tabs

--- crimsononline/content/search_indexes.py
+++ crimsononline/content/search_indexes.py
@@ -4,14 +4,14 @@ from haystack import indexes
 from crimsononline.content.models import Article
 
 class ArticleIndex(indexes.SearchIndex):
-	text = indexes.CharField(document=True, model_attr='teaser', use_template=True)
-	pub_date = indexes.DateTimeField(model_attr='issue__issue_date')
-	
-	def get_updated_field(self):
-		return 'modified_on'
-		
-	def get_queryset(self):
-	    return Article.objects.filter(issue__issue_date__gte=datetime.date(1900,1,1))
+    text = indexes.CharField(document=True, model_attr='teaser', use_template=True)
+    pub_date = indexes.DateTimeField(model_attr='issue__issue_date')
+    
+    def get_updated_field(self):
+        return 'modified_on'
+        
+    def get_queryset(self):
+        return Article.objects.filter(issue__issue_date__gte=datetime.date(1900,1,1))
     
     def should_update(self, instance **kwargs):
         return False
