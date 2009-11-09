b89fe2ffc054904de990714db086d3ac8845c2b0
lazovich <lazovich@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-11-09 19:53:53 +0000

    Javascript file for getting rid of enter submit in form

---
+++ crimsononline/static/scripts/noenter.js
@@ -0,0 +1,9 @@
+//Function to eat up the enters that people put into forms so it doesn't
+//automatically submit.
+function stopRKey(evt) {
+  var evt = (evt) ? evt : ((event) ? event : null);
+  var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+  if ((evt.keyCode == 13) && (node.type=="text"))  {return false;}
+}
+
+document.onkeypress = stopRKey; 
