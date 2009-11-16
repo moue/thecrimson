4a139c043de28b99afd666b5b2455b2e81b03e83
sstelmach <sstelmach@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2009-11-16 04:46:37 +0000

    sitemap (non-xml)

--- crimsononline/content/views.py
+++ crimsononline/content/views.py
@@ -31,7 +31,7 @@ def sitemap(request, year=None, month=None):
     if year is None and month is None:
         ordered = Article.objects.order_by("issue__issue_date")
         oldest = ordered[0].issue.issue_date.year
-        newest = ordered[len(ordered)-1].issue.issue_date.year
+        newest = ordered[ordered.count()-1].issue.issue_date.year
         years = range(oldest,newest+1)
         months = [("%02d"%x) for x in range(1,13)]
         return render_to_response("sitemap/sitemap_base.html",{'years':years,'months':months})
