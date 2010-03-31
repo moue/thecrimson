8b08f9e5f3d78a984b864e0cb4a6de9d33746a97
krakip <krakip@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2010-03-31 01:42:37 +0000

    Sports blog widget

--- crimsononline/content/templatetags/flyby_preview.py
+++ crimsononline/content/templatetags/flyby_preview.py
@@ -1,6 +1,6 @@
 from django import template
 from django.template.loader import render_to_string
-from crimsononline.content.models import Content, Section, Article
+from crimsononline.content.models import Content, Section, Article, ContentGroup
 
 register = template.Library()
 
@@ -21,3 +21,20 @@ def do_flyby(parser, token, nopreview=False):
     return FlyByNode()
 
 register.tag('flyby_preview', do_flyby)
+
+class BlogNode(template.Node):
+    """
+    Generates a blog widget thing that doesn't have the Flyby logo.
+    TODO: Generalize this so that it works for contentgroups that are not the sports blog.
+    """
+    def render(self, context):
+        sb = ContentGroup.objects.get(name='The Back Page')
+        self.posts = Article.objects.recent.filter(group=sb)[:4]
+        return render_to_string('templatetag/blog_preview.html', 
+            {'posts': self.posts,})
+    
+
+def do_blog(parser, token, nopreview=False):
+    return BlogNode()
+
+register.tag('blog_preview', do_blog)
