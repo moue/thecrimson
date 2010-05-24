5921c9cc56b23175633d85aa503b9168e61b6974
krakip <krakip@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2010-05-24 03:23:35 +0000

    So much for that.

--- crimsononline/content/models.py
+++ crimsononline/content/models.py
@@ -109,7 +109,7 @@ class ContentManager(models.Manager):
 
     @property
     def recent(self):
-        return self.get_query_set().order_by('-created_on')
+        return self.get_query_set().order_by('-issue__issue_date')
 
     def prioritized(self, recents=7):
         """Order by (priority / days_old).
