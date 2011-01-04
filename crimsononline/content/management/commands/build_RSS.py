9b0cf134468d27b54e68e41f06e993154cd3913f
ejb3800@aol.com <ejb3800@aol.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-01-04 22:52:10 +0000

    added functionality to build individual items, running tests now

--- crimsononline/content/management/commands/build_RSS.py
+++ crimsononline/content/management/commands/build_RSS.py
@@ -1,6 +1,26 @@
 from django.core.management.base import NoArgsCommand
+from models import *
 
 class Command(NoArgsCommand):
-    help = "this is some help text"
+    help = "This command will build all of the RSS feeds"
     def handle_noargs(self, **options):
-        return "this is a test"
+        
+        def buildItem(self, obj):
+            #helper function to build individual items
+            rStr = "<item>"
+            rStr += "<title>"+ obj.headline+"</title>"
+            rStr += "<link>"+obj.get_absolute_url()+"</link>"
+            rStr += "<description>" + obj.teaser+"</description>"
+            rStr += "<dc:creator xmlns:dc='http://purl.org/dc/elements/1.1/'>"
+            for i in obj.contributors.all():
+                rStr += i.first_name + " " +i.last_name +", "
+            rStr = rStr[:-2]
+            rStr += "</dc:creator>"
+            rStr += "<guid>"+obj.get_absolute_url()+"</guid>"
+            rStr += "</item>"
+            return rStr
+            
+        test = Article.objects.exclude(section=6).order_by('-issue__issue_date')[:1]
+        #build the top news feed
+        
+        return buildItem(test)
