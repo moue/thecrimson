ce293f8393ecd6c448326e65ae630cfc88a733ca
ejb3800@aol.com <ejb3800@aol.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-01-04 22:19:26 +0000

    take 2

--- crimsononline/content/management/commands/build_RSS.py
+++ crimsononline/content/management/commands/build_RSS.py
@@ -1,6 +1,6 @@
 from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
-help = “Describe the Command Here”
-def handle_noargs(self, **options):
-    return "this is a test"
+    help = “Describe the Command Here”
+    def handle_noargs(self, **options):
+        return "this is a test"
