3bfe50c867c899ad4d034cafc1aa9ec3105c1044
ejb3800@aol.com <ejb3800@aol.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-01-05 22:08:04 +0000

    added the code to complete the rest of the feeds.

--- crimsononline/content/management/commands/build_RSS.py
+++ crimsononline/content/management/commands/build_RSS.py
@@ -33,20 +33,46 @@ class Command(NoArgsCommand):
             rStr += "<language>en-us</language>"
             rStr += "<lastBuildDate>" +str(datetime.datetime.now())+"</lastBuildDate>"
             return rStr
-            
+        
+        def writeFeed(loc, content):
+            f1 = codecs.open(loc,'r+','utf-8') #must specifically encode the file as utf-8 or you'll run into problems
+            f1.write(content)
+            f1.close()
+        
         fileStart = '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel>'
         fileEnd = '</channel></rss>'
 
         #build the top news feed
-        topNewsFeed = fileStart + buildHeaderInfo(" Top Stories","The top Crimson articles")
-        tnStories = Article.objects.exclude(section=6).order_by('-issue__issue_date')[:numberOfStories]
+        topNewsFeed = fileStart + buildHeaderInfo(" Top Stories","The Top Crimson Articles")
+        tnStories = Article.objects.exclude(section=6).order_by('-issue__issue_date').exclude(priority__lte=5)[:numberOfStories]
         for x in tnStories:
             topNewsFeed += buildItem(x)
             
         topNewsFeed += fileEnd
         
-        f1 = codecs.open('/home/sites/crimson/crimsononline/feeds/TopNews.xml','r+','utf-8')
-        f1.write(topNewsFeed)
-        f1.close()
+        writeFeed('/home/sites/crimson/crimsononline/feeds/TopNews.xml', topNewsFeed)
         
+        #build the latest news feed
+        latestNewsFeed = fileStart + buildHeaderInfo(" Top Stories","The Latest Crimson Articles")
+        latestItems = Article.objects.exclude(section=6).order_by('-issue__issue_date')[:numberOfStories]
+        for x in latestItems:
+            latestNewsFeed += buildItem(x)
+        
+        latestNewsFeed += fileEnd
+        
+        writeFeed('/home/sites/crimson/crimsononline/feeds/LatestNews.xml', latestNewsFeed)
+        
+        #write the section feeds
+        sections = ['arts','opinion','fm','news','sports','flyby']
+        
+        for i in sections:
+            sectionObj = Section.objects.get(name__iexact=i)
+            feedText = fileStart + buildHeaderInfo(" Top Stories","The Latest Crimson Articles in %s" % i)
+            feedItems = Article.objects.filter(section__exact=sectionObj.pk).order_by('-issue__issue_date')[:numberOfStories]
+            for x in feedItems:
+                feedText += buildItem(x)
+            
+            feedText += fileEnd
+            writeFeed('/home/sites/crimson/crimsononline/feeds/'+i+'.xml', feedText)
+            
         #return topNewsFeed
