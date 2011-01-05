277390f5e32269a45ef62fb6c947ef91c9301565
ejb3800@aol.com <ejb3800@aol.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-01-05 21:08:02 +0000

    should be fixed now, encoding all descriptions into utf-8

--- crimsononline/content/management/commands/build_RSS.py
+++ crimsononline/content/management/commands/build_RSS.py
@@ -13,7 +13,7 @@ class Command(NoArgsCommand):
             rStr = "<item>"
             rStr += "<title>"+ obj.headline+"</title>"
             rStr += "<link>"+obj.get_absolute_url()+"</link>"
-            rStr += "<description>" + repr(obj.teaser)+"</description>"
+            rStr += "<description>" + repr(obj.teaser).encode("utf-8")+"</description>"
             rStr += "<dc:creator xmlns:dc='http://purl.org/dc/elements/1.1/'>"
             for i in obj.contributors.all():
                 rStr += i.first_name + " " +i.last_name +", "
