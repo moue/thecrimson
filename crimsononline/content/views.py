16e247c5e7e92f1ebde76a0a725c2a3226ce3af3
sophiechanga <sophiechanga@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-01-28 02:21:59 +0000

    reverting changes

--- crimsononline/content/views.py
+++ crimsononline/content/views.py
@@ -136,7 +136,7 @@ def writer(request, pk, f_name, m_name, l_name, page=1, sections=None, types=Non
     )
 
     w.number_of_articles = Article.objects.filter(contributors=w).count()
-    #d = paginate(f.pop('content'), page, 10)
+    d = paginate(f.pop('content'), page, 10)
     d.update({'page': page, 'url_base': url_base})
     w.last_update = Content.objects.filter(contributors=w).aggregate(Max('issue__issue_date'))['issue__issue_date__max']
     d.update({'writer': w, 'url': REMOVE_P_RE.sub(request.path, '')})
