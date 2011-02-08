f2674971daabab52220e7df37cdcfb4ae86782d8
ejb3800@aol.com <ejb3800@aol.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-02-08 08:48:22 +0000

    updated most read articles to use google analytics -EJ

--- crimsononline/content/management/commands/doMostRead.py
+++ crimsononline/content/management/commands/doMostRead.py
@@ -2,36 +2,53 @@ from django.core.management.base import NoArgsCommand
 from crimsononline.content.models import *
 import datetime
 from django.db import connection
+import gdata.analytics.client
+import gdata.sample_util
 
 class Command(NoArgsCommand):
     help = "This command will create a new most read article row"
     def handle_noargs(self, **options):
-        cursor = connection.cursor()
-        seven_days_ago = " ( NOW() - INTERVAL 7 day ) "
-        tableStr = ""
-        limitStr = ""
-        #as was pointed out the in the original comments in top_articles.py, this is probably a terrible way to do this, but given time constraints, doing it like this should resolve the stability issues we had
+        SOURCE_APP_Name = 'thecrimson.com'
+        my_client = gdata.analytics.client.AnalyticsClient(source=SOURCE_APP_Name)
         
-        sqlstatement = "SELECT DISTINCT content_article.content_ptr_id, SUM(content_contenthits.hits) AS hitnum FROM content_article, " \
-                           "content_content, content_contenthits" + tableStr + \
-                           " WHERE content_content.id = content_article.content_ptr_id " \
-                           " AND content_contenthits.content_id = content_content.id " \
-                           " AND content_content.pub_status = 1 " \
-                           " AND content_contenthits.date >" + seven_days_ago + limitStr + \
-                           " GROUP BY content_contenthits.content_id ORDER BY hitnum DESC LIMIT 5"
-        
-        cursor.execute(sqlstatement)
-        mostreadarticle = cursor.fetchall()
         try:
-            mostreadarticle = [Content.objects.get(pk=x[0]).child for x in mostreadarticle]
-        except:
-            mostreadarticle = None
+          my_client.client_login("thc.analytics","lizwoodley", source=SOURCE_APP_Name, service=my_client.auth_service)
+        except gdata.client.BadAuthentication:
+          exit('Invalid user credentials given.')
+        except gdata.client.Error:
+          exit('Login Error')
         
-        if mostreadarticle != None and len(mostreadarticle) > 4:
-            mr = MostReadArticles(article1=mostreadarticle[0], article2=mostreadarticle[1], article3=mostreadarticle[2], article4=mostreadarticle[3], article5=mostreadarticle[4]) 
+        now = datetime.datetime.now()
+        dateChange = datetime.timedelta(days=-2)
+        startDate = now + dateChange
+        
+        data_query = gdata.analytics.client.DataFeedQuery({
+            'ids': 'ga:509545',
+            'start-date': startDate.strftime("%Y-%m-%d"),
+            'end-date': now.strftime("%Y-%m-%d"),
+            'dimensions': 'ga:pagePath',
+            'metrics': 'ga:pageviews,ga:uniquePageviews,ga:timeOnPage,ga:bounces,ga:entrances,ga:exits',
+            'sort': '-ga:pageviews',
+            'filters': 'ga:pagePath=@/article/',
+            'max-results': '5'})
+
+        self.feed = my_client.GetDataFeed(data_query)
+        slugList = []
+        for e in self.feed.entry:
+            for m in e.dimension:
+                tempStr = m.value[:-1]
+                slugList.append(tempStr[tempStr.rfind("/")+1:])
+        
+        articleList = []
+        for s in slugList:
+            try:
+                articleList.append(Content.objects.get(slug=s))
+            except:
+                str1 ="DoNothing"
+        
+        if len(articleList) == 5:
+            mr = MostReadArticles(article1=articleList[0], article2=articleList[1], article3=articleList[2], article4=articleList[3], article5=articleList[4])
             mr.save()
-            #return "Success"
-        else:
-            return "Failure"
-            #var = "f"
-        #return MostReadArticles.objects.order_by('create_date')[0]
+            
+        #return articleList
+        
