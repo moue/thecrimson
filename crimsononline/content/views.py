2e83897ace581c7774d3285516c5e327013cf40f
sophiechanga <sophiechanga@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-03-04 02:04:44 +0000

    revert

--- crimsononline/content/views.py
+++ crimsononline/content/views.py
@@ -608,8 +608,6 @@ def get_content_group(request, gtype, gname, page=1, tags=None):
     if not cg:
         raise Http404
     c = cg.content.all()
-    baseball = Article.objects.filter(tags='Baseball' and group=cg)
-    softball = Article.objects.filter(tags='Softball' and group=cg)
     # check if flyby content group - if so, just pass to flyby view
     if cg.section == Section.objects.get(name='flyby'):
         return section_flyby(request, page, cg=cg)
