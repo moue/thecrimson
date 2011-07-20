bc58df8fa6f14e6623bbf84b5ec475c0a1609292
sophiechanga@gmail.com <sophiechanga@gmail.com@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-07-20 21:29:36 +0000

    try again

--- crimsononline/content/management/commands/build_RSS.py
+++ crimsononline/content/management/commands/build_RSS.py
@@ -18,8 +18,10 @@ class Command(NoArgsCommand):
             #desc = desc.encode("utf-8")
             rStr += "<description>" + desc+"</description>"
             rStr += "<pubDate>"+str(obj.created_on)+"</pubDate>"
-            for rc in obj.rel_content.all():
-                rStr += "<media:content url='"+rc.absolute_url+"' />"
+            if obj.main_rel_content.content_type == ContentType.objects.get(name="image"):
+                rStr += "<media:content url='http://thecrimson.com"+obj.main_rel_content.get_absolute_url()+"' />"
+            #for i in obj.rel_content.all():
+            #   rStr += "<media:content url='http://thecrimson.com"+i.related_content.get_absolute_url()+"' />"
             rStr += "<dc:creator xmlns:dc='http://purl.org/dc/elements/1.1/'>"
             for i in obj.contributors.all():
                 rStr += i.first_name + " " +i.last_name +", "
