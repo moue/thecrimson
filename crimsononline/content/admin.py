64010bb930ee498d8e133ae7507a5954800e8735
ebensing38 <ebensing38@6bc974aa-514d-0410-9512-6b4bf00f8a56>
2011-10-09 21:00:38 +0000

    fix for dumb count(*) queries in admin

--- crimsononline/content/admin.py
+++ crimsononline/content/admin.py
@@ -1403,10 +1403,42 @@ class FeaturePackageAdmin(admin.ModelAdmin):
                 #logging.debug(str(contents))
         """  
         
+class ApproxCountQuerySet(QuerySet): 
+    """Counting all rows is very expensive on large Innodb tables. This 
+    is a replacement for QuerySet that returns an approximation if count()
+    is called with no additional constraints. In all other cases it should
+    behave exactly as QuerySet. 
+    """ 
+ 
+    def count(self): 
+        # Code from django/db/models/query.py 
         
+        if self._result_cache is not None and not self._iter: 
+            return len(self._result_cache) 
+ 
+        query = self.query 
+        if (not query.where) and \ 
+                query.high_mark is None and \ 
+                query.low_mark == 0 and \ 
+                (not query.select) and \ 
+                (not query.group_by) and \ 
+                (not query.having) and \ 
+                (not query.distinct): 
+            # If query has no constraints, we would be simply doing
+            # "SELECT COUNT(*) FROM foo". Monkey patch so the we 
+            # get an approximation instead. 
+            cursor = connections[self.db].cursor() 
+            cursor.execute("SHOW TABLE STATUS LIKE %s", 
+                    (self.model._meta.db_table,)) 
+            return cursor.fetchall()[0][4] 
+        else: 
+            return self.query.get_count(using=self.db) 
         
-        
-    
+class countFixAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        qs = super(countFixAdmin, self).queryset(request)
+        return qs._clone(klass=ApproxCountQuerySet)
+
 admin.site.register(FeaturePackage, FeaturePackageAdmin)
 #admin.site.register(FeaturePackageSection, FeaturePackageSectionAdmin)
 
