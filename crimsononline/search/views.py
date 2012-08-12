924ce5c9d630ee1a7ad015c6720cc3684ef06260
Kevin Grinberg <kevin@activefrequency.com>
2012-08-12 12:46:55 -0400

    Don't blow up if not using Haystack

--- crimsononline/search/views.py
+++ crimsononline/search/views.py
@@ -1,49 +1,53 @@
+from django.conf import settings
 from django.shortcuts import render_to_response
 from crimsononline.content.models import Contributor, Tag
-from haystack.views import SearchView
-from haystack.query import SearchQuerySet
 
 MAX_MCONTRIBS = 20
 
-class AjaxSearchView(SearchView):
-    def __name__(self):
-        return "AjaxSearchView"
 
-    def create_response(self):
+if settings.HAYSTACK:
+    from haystack.views import SearchView
+    from haystack.query import SearchQuerySet
     
-        """
-        Generates the actual HttpResponse to send back to the user.
-        """
-    
-        qu =  self.request.GET.get('q','')
-        if qu:
-            contributors_qs = SearchQuerySet().models(Contributor)
-            tags_qs = SearchQuerySet().models(Tag)
-            matching_contributors = (contributors_qs.auto_query(qu))[:MAX_MCONTRIBS]
-            matching_tags = tags_qs.auto_query(qu)
-        else:
-            matching_contributors = None
-            matching_tags = None
-    
-        (paginator, page) = self.build_page()
-    
-        context = {
-            'query': self.query,
-            'form': self.form,
-            'page': page,
-            'paginator': paginator,
-            'mcontributors': matching_contributors,
-            'mtags': matching_tags,
-            'is_search': qu
-        }
-        context.update(self.extra_context())
+    class AjaxSearchView(SearchView):
+        def __name__(self):
+            return "AjaxSearchView"
 
-        if self.request.GET.has_key('ajax'):
-            t = 'ajax/search_results_page.html'
-        else:
-            t = self.template
-        return render_to_response(t, context, 
-            context_instance=self.context_class(self.request))
+        def create_response(self):
+    
+            """
+            Generates the actual HttpResponse to send back to the user.
+            """
+    
+            qu =  self.request.GET.get('q','')
+            if qu:
+                contributors_qs = SearchQuerySet().models(Contributor)
+                tags_qs = SearchQuerySet().models(Tag)
+                matching_contributors = (contributors_qs.auto_query(qu))[:MAX_MCONTRIBS]
+                matching_tags = tags_qs.auto_query(qu)
+            else:
+                matching_contributors = None
+                matching_tags = None
+    
+            (paginator, page) = self.build_page()
+    
+            context = {
+                'query': self.query,
+                'form': self.form,
+                'page': page,
+                'paginator': paginator,
+                'mcontributors': matching_contributors,
+                'mtags': matching_tags,
+                'is_search': qu
+            }
+            context.update(self.extra_context())
+
+            if self.request.GET.has_key('ajax'):
+                t = 'ajax/search_results_page.html'
+            else:
+                t = self.template
+            return render_to_response(t, context, 
+                context_instance=self.context_class(self.request))
     
 def searchView(request):
     #qu =  self.request.GET.get('q','')
