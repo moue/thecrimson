56536e842df696a45df42b4e7e5983a6447f1149
Kevin Grinberg <kevin@activefrequency.com>
2012-08-13 14:57:56 -0400

    Need this to use messages framework

--- crimsononline/settings.py
+++ crimsononline/settings.py
@@ -93,6 +93,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.doc.XViewMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'crimsononline.mware.supercrisismode.SuperCrisisMode',
